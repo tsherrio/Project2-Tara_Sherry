80bba0a73e2a7ae13113c00b927c6597d640ca3e
Tara Sherry <tara.sherry@mycit.ie>
2019-11-26 01:23:51 +0000

    Removed uneccessary links, adjusted hierarchy

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,13 +1,22 @@
-define(function() {var keywords=[{w:"About",p:["p0"]},{w:"this",p:["p0"]},{w:"publication",p:["p0"]},{w:"Applications",p:["p1"]},{w:"of",p:["p1"]},{w:"IBM",p:["p1","p3","p6","p7"]},{w:"StoredIQ",p:["p1","p3","p6","p7"]},{w:"Solution",p:["p2"]},{w:"components",p:["p2","p3"]},{w:"Cover",p:["p4"]},{w:"page",p:["p4"]},{w:"***",p:["p5"]},{w:"product",p:["p6"]},{w:"library",p:["p6"]},{w:"Contacting",p:["p7"]},{w:"customer",p:["p7"]},{w:"support",p:["p7"]}];
+define(function() {var keywords=[{w:"About",p:["p0"]},{w:"this",p:["p0"]},{w:"publication",p:["p0"]},{w:"Applications",p:["p1"]},{w:"of",p:["p1"]},{w:"IBM",p:["p1","p2","p3","p4","p5","p6","p7","p11","p14","p15","p16"]},{w:"StoredIQ",p:["p1","p2","p3","p4","p5","p6","p7","p11","p14","p16"]},{w:"Data",p:["p2","p3","p4"]},{w:"Server",p:["p2"]},{w:"Workbench",p:["p3"]},{w:"Desktop",p:["p4"]},{w:"Collector",p:["p4"]},{w:"Policy",p:["p5"]},{w:"Manager",p:["p5"]},{w:"Administrator",p:["p6"]},{w:"Insights",p:["p7"]},{w:"Open",p:["p8"]},{w:"Virtual",p:["p8"]},{w:"Appliance",p:["p8"]},{w:"(OVA)",p:["p8"]},{w:"configuration",p:["p8"]},{w:"requirements",p:["p8"]},{w:"Planning",p:["p9"]},{w:"for",p:["p9"]},{w:"deployment",p:["p9"]},{w:"Solution",p:["p10"]},{w:"components",p:["p10","p11"]},{w:"***",p:["p12"]},{w:"Cover",p:["p13"]},{w:"page",p:["p13"]},{w:"product",p:["p14"]},{w:"library",p:["p14"]},{w:"Contacting",p:["p15","p16"]},{w:"customer",p:["p16"]},{w:"support",p:["p16"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6];
-ph["p2"]=[7, 8];
-ph["p3"]=[5, 6, 8];
-ph["p4"]=[9, 10];
-ph["p5"]=[11];
-ph["p6"]=[5, 6, 12, 13];
-ph["p7"]=[14, 5, 6, 15, 16];
+ph["p2"]=[5, 6, 7, 8];
+ph["p3"]=[5, 6, 7, 9];
+ph["p4"]=[5, 6, 10, 7, 11];
+ph["p5"]=[5, 6, 12, 13];
+ph["p6"]=[5, 6, 14];
+ph["p7"]=[5, 6, 15];
+ph["p8"]=[16, 17, 18, 19, 20, 21];
+ph["p9"]=[22, 23, 24];
+ph["p10"]=[25, 26];
+ph["p12"]=[27];
+ph["p11"]=[5, 6, 26];
+ph["p14"]=[5, 6, 30, 31];
+ph["p13"]=[28, 29];
+ph["p16"]=[32, 5, 6, 33, 34];
+ph["p15"]=[32, 5];
      return {
          keywords: keywords,
          ph: ph
